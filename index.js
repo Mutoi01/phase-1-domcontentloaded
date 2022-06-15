7cd5417b2382b33ef2f8a3fb6d6a904a6defd76f
Mutoi01 <pngando9@gmail.com>
2022-06-15 06:25:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",function(){
+  console.log("DOM loaded");
+  changeText()
+})
+
+function changeText(){
+  let mod = document.querySelector('p');
+  mod.textContent = "This is really cool!"
+}
